88a2dbb36ce8134ac6ad74d26b43653cf0e0b142
Алексей <electrifyx@gmail.com>
2023-04-24 14:39:41 +0300

    edit task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+import Statictics from './Statictics/Statictics';
+
+import { useState } from 'react';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  function handleClickButton(event) {
+    switch (event) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      
+      default:
+        return;
+    }
+  }
+
+  const totalFeedback = good + neutral + bad;
+  const percentage = Math.round((good / totalFeedback) * 100);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          buttonClick={handleClickButton}
+        />
+      </Section>
+
+      <Section title="Statictics">
+        {!totalFeedback ? (
+          <Notification message={'There is no feedback'} />
+        ) : (
+          <Statictics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positiveFeedback={percentage}
+          />
+        )}
+      </Section>
     </div>
   );
-};
+}
+
